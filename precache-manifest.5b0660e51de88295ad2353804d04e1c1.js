a2fd5708a2d6523497114194915308593b654eb2
John Woodruff <johnwoodruff91@gmail.com>
2019-02-07 10:19:43 -0700

    Updates

---
+++ precache-manifest.5b0660e51de88295ad2353804d04e1c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "f3adb3467457bc85aa74344cfce49366",
+    "url": "/domo-phoenix/static/media/domo-logo.f3adb346.jpg"
+  },
+  {
+    "revision": "707c598aa7d805eaa914",
+    "url": "/domo-phoenix/static/js/runtime~main.707c598a.js"
+  },
+  {
+    "revision": "83248010c87bf905cbc0",
+    "url": "/domo-phoenix/static/js/main.83248010.chunk.js"
+  },
+  {
+    "revision": "f96bf53ea8c04199b10e",
+    "url": "/domo-phoenix/static/js/1.f96bf53e.chunk.js"
+  },
+  {
+    "revision": "83248010c87bf905cbc0",
+    "url": "/domo-phoenix/static/css/main.998fd887.chunk.css"
+  },
+  {
+    "revision": "f96bf53ea8c04199b10e",
+    "url": "/domo-phoenix/static/css/1.fee09d23.chunk.css"
+  },
+  {
+    "revision": "ebd67dbbb29e00ef16ecfd890a1398ec",
+    "url": "/domo-phoenix/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/domo-phoenix/precache-manifest.9544a4505ff951a2a8a73a2fbbd74e2d.js"
+  "/domo-phoenix/precache-manifest.5b0660e51de88295ad2353804d04e1c1.js"
 );
 
 workbox.clientsClaim();

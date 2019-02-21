b9c4eefa5739b3e1c51e848401648163e5d7169e
Tanner Bindrup <tanner.bindrup@domo.com>
2019-02-20 23:43:35 -0700

    Remove erroneous console.log calls

--- src/bundleEntry_noMaps.js
+++ src/bundleEntry_noMaps.js
@@ -1,4 +1,2 @@
 import { PhoenixChart, PHOENIX_CHART_TYPE, PHOENIX_DATA_TYPE, PHOENIX_MAPPING } from './index_noMaps';
-console.log({ PhoenixChart, PHOENIX_CHART_TYPE, PHOENIX_DATA_TYPE, PHOENIX_MAPPING })
 window.DomoPhoenix = { PhoenixChart, PHOENIX_CHART_TYPE, PHOENIX_DATA_TYPE, PHOENIX_MAPPING };
-console.log(window.DomoPhoenix)

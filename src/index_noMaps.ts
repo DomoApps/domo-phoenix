8958666b56d94d4743f54eb88cedcd0d4f1ac1ee
Tanner Bindrup <tanner.bindrup@domo.com>
2019-02-21 11:06:08 -0700

    Get rid of the changes that strip out maps per johnbwoodruff's request

--- src/index_noMaps.ts
+++
@@ -1,6 +0,0 @@
-export * from './chart/chart_noMaps';
-export * from './enums/phoenix-chart-type_noMaps';
-export * from './enums/phoenix-data-type';
-export * from './enums/phoenix-mapping';
-export * from './interfaces/phoenix-chart-data';
-export * from './interfaces/phoenix-chart-options';

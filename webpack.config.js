4c460b774845c99f664530bc950d6bd50c2c7ff8
Tanner Bindrup <tbindrup1221@gmail.com>
2019-02-21 14:51:03 -0700

    Add webpack build to compile library into single file (#3)
    * Update README to refer to StarterKit
    
    * Update README.md
    
    Co-Authored-By: th3uiguy <453367+th3uiguy@users.noreply.github.com>
    
    * Add webpack build to compile library into single file
    
    * Fix incorrect entry in webpack config files
    
    * Remove erroneous console.log calls
    
    * Remove dist files from tracking
    
    * Get rid of babel and use all Typescript instead
    
    * Remove erroneous dependency
    
    * Get rid of the changes that strip out maps per johnbwoodruff's request
    
    * Adding support for BackgroundColor and TextColor
    
    * Update package.json to add dist to build command per johnbwoodruff
    
    Co-Authored-By: tbindrup <tbindrup1221@gmail.com>
    
    * Update webpack.config.js to combine output into build directory per johnbwoodruff
    
    Co-Authored-By: tbindrup <tbindrup1221@gmail.com>
    
    * Fix incorrect entry in webpack config files
    
    * Remove dist files from tracking
    
    * Get rid of babel and use all Typescript instead
    
    * Get rid of the changes that strip out maps per johnbwoodruff's request
    
    * Update package.json to add dist to build command per johnbwoodruff
    
    Co-Authored-By: tbindrup <tbindrup1221@gmail.com>
    
    * Update webpack.config.js to combine output into build directory per johnbwoodruff
    
    Co-Authored-By: tbindrup <tbindrup1221@gmail.com>
    
    * Make sure all build output files are put under the build directory

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+const packageJson = require('./package.json');
+
+const bannerComment = [
+  `domoPhoenix.js v${packageJson.version}`,
+  'Optional utility library for Custom Apps'
+].join('\n');
+
+const config = {
+  context: __dirname + '/src',
+
+  entry: './bundleEntry.ts',
+
+  output: {
+    path: path.join(__dirname, 'build', 'global'),
+    publicPath: '/build/global',
+    filename: 'domoPhoenix.js',
+    library: 'domoPhoenix',
+    libraryTarget: 'umd'
+  },
+
+  plugins: [
+    new webpack.BannerPlugin(bannerComment)
+  ],
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: 'ts-loader'
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx']
+  },
+
+  devtool: 'source-map'
+};
+
+module.exports = config;
